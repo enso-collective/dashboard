7baa578c5f19ad22ce9ba739e8a497fefeb8b7ed
atlasmoth <ebuka422@gmail.com>
2024-02-17 20:12:05 +0100

    added bg image and improved ui/added quests page

---
+++ components/empty.tsx
@@ -0,0 +1,12 @@
+export default function Empty() {
+  return (
+    <div className="bg-denver min-h-screen">
+      <main className="p-4 md:p-10 mx-auto max-w-7xl flex justify-center items-center">
+        <div
+          id="render-privy"
+          className="z-[2] min-w-[360px] max-w-[360px] pt-8 md:mx-0 md:pt-0"
+        />
+      </main>
+    </div>
+  );
+}
