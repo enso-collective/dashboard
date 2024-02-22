76274023805d69e300704884591e39b905af637a
Sean Brennan <sean@relay.cc>
2024-02-22 16:29:24 +0100

    setConfig fix

--- components/profilePage.tsx
+++ components/profilePage.tsx
@@ -65,7 +65,7 @@ export default function ProfilePage() {
         }
       }));
     };
-  }, [setConfig]);
+  }, []);
   const [activeWallet, setActiveWallet] = useState<WalletWithMetadata | null>(
     null
   );
